8f88c37320b287073f14e09ab6bba20a2d3bb3d5
chen <chen_x23@qq.com>
2020-11-15 10:31:20 +0800

    add userName

--- src/main/java/com/fosu/User.java
+++ src/main/java/com/fosu/User.java
@@ -3,6 +3,7 @@ package com.fosu;
 public class User {
     private int id;
     private String name;
+    private String phone;
 
     public User(int id) {
         this.id = id;
@@ -11,5 +12,11 @@ public class User {
     public User(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public User(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
     }
 }
